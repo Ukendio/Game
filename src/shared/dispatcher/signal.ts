d4e5c5b1c0610b5c674d208166ca5f1f93789d5c
Marcus <Ukendio@gmail.com>
2021-05-22 17:06:49 +0200

    added dispatcher

---
+++ src/shared/dispatcher/signal.ts
@@ -0,0 +1,62 @@
+interface Listener {
+	handler: Callback;
+	disconnected: boolean;
+	connectTraceback: Callback;
+	disconnectTraceback: string;
+	next: Listener;
+}
+
+class Signal {
+	private _currentListHead = undefined! as Listener;
+
+	connect(handler: Callback) {
+		const listener: Listener = {
+			handler: handler,
+			disconnected: false,
+			connectTraceback: debug.traceback,
+			disconnectTraceback: undefined!,
+			next: this._currentListHead,
+		};
+
+		const disconnect = () => {
+			if (listener.disconnected) {
+				throw `Listener connected at: \n${listener.connectTraceback}\nwas already disconnected at listener.disconnectTraceback`;
+			}
+
+			listener.disconnected = true;
+			listener.disconnectTraceback = debug.traceback();
+
+			if (this._currentListHead === listener) {
+				this._currentListHead = listener.next;
+			} else {
+				let previous = this._currentListHead;
+
+				while (previous && previous.next !== listener) {
+					previous = previous.next;
+				}
+
+				if (previous) {
+					previous.next = listener.next;
+				}
+			}
+		};
+
+		this._currentListHead = listener;
+		return {
+			listener: listener,
+			disconnect: disconnect,
+		};
+	}
+
+	fire(...args: unknown[]) {
+		let listener = this._currentListHead;
+		while (listener !== undefined) {
+			if (!listener.disconnected) {
+				listener.handler(...args);
+			}
+			listener = listener.next;
+		}
+	}
+}
+
+export = Signal;
---
+++ src/shared/dispatcher/singleEvent.ts
@@ -0,0 +1,39 @@
+class SingleEvent {
+	private _listener = undefined! as Callback;
+	private _promise = undefined! as Promise<unknown>;
+	constructor(
+		executor: (
+			dispatch: Callback,
+		) => (
+			resolve: (value: unknown) => void,
+			reject: (reason?: unknown) => void,
+			onCancel: (abortHandler: () => void) => boolean,
+		) => void,
+	) {
+		const dispatch = () => {
+			if (this._listener !== undefined) {
+				coroutine.wrap(this._listener)();
+			}
+		};
+		this._promise = Promise.defer((resolve) => {
+			resolve(new Promise(executor(dispatch)).then(() => (this._listener = undefined!)));
+		});
+	}
+
+	connect(handler: Callback) {
+		assert(this._listener === undefined, "Dispatcher is already used up");
+		assert(this._promise.getStatus() === "Started", "Dispatcher is already used up");
+
+		this._listener = handler;
+		const disconnect = () => {
+			this._promise.cancel();
+			this._listener = undefined!;
+		};
+
+		return {
+			disconnect: disconnect,
+		};
+	}
+}
+
+export = SingleEvent;
