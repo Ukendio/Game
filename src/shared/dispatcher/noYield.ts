4b45651021f0bc8e27239d4902354426d538c16a
Marcus <Ukendio@gmail.com>
2021-05-23 20:34:59 +0200

    export noyield from dispatcher

---
+++ src/shared/dispatcher/noYield.ts
@@ -0,0 +1,17 @@
+function resultHandler(co: thread, ok: boolean, ...results: unknown[]) {
+	if (!ok) {
+		error(debug.traceback(co, ...((results as never) as string)), 2);
+	}
+
+	if (coroutine.status(co) !== "dead") {
+		error(debug.traceback(co, "Attempted to yield inside changed event!"), 2);
+	}
+
+	return results as LuaTuple<[unknown, unknown]>;
+}
+
+export function noYield<T extends (...args: never[]) => unknown>(callback: T, ...args: Parameters<T>) {
+	const co = coroutine.create(callback);
+
+	return resultHandler(co, coroutine.resume(co, ...args) as never);
+}
