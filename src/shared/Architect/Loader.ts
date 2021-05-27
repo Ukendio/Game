1a72ad8278f074a6dc82d24acc3c0eccdb85e0dc
Marcus <Ukendio@gmail.com>
2021-05-27 16:36:20 +0200

    return map

--- src/shared/Architect/Loader.ts
+++ src/shared/Architect/Loader.ts
@@ -3,4 +3,6 @@ import { ReplicatedStorage, Workspace } from "@rbxts/services";
 export function mapLoadAsync(mapName: string) {
 	const map = ReplicatedStorage.TS.Architect.maps.FindFirstChild(mapName) as Model;
 	map.Parent = Workspace;
+
+	return map;
 }
