765a1a69ff42be518e53ef4410e9c152fc25ccd7
Marcus <Ukendio@gmail.com>
2021-05-27 07:43:56 +0200

    added utility for tables

--- src/shared/tableUtil.ts
+++ src/shared/tableUtil.ts
@@ -1,7 +1,16 @@
-export function copyShallow<T>(obj: WritableProperties<T>): T {
+export function copyShallow<T extends object>(obj: T): T {
 	const t = {} as T;
 	for (const [k, v] of pairs(obj)) {
 		t[k as never] = v as never;
 	}
-	return t as T;
+	return t;
+}
+
+export function getKeys<T extends object>(obj: T) {
+	const t = new Array<keyof T>();
+	for (const [k] of pairs(obj)) {
+		t.push(k);
+	}
+
+	return t;
 }
