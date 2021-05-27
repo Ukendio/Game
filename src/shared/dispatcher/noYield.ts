684fe76f0e8a2392342f92e9f7239b6ac79bdee5
Marcus <Ukendio@gmail.com>
2021-05-27 19:46:43 +0200

    replace error with warn

--- src/shared/dispatcher/noYield.ts
+++ src/shared/dispatcher/noYield.ts
@@ -1,10 +1,10 @@
 function resultHandler(co: thread, ok: boolean, ...results: unknown[]) {
 	if (!ok) {
-		error(debug.traceback(co, ...(results as string[])), 2);
+		warn(debug.traceback(co, ...(results as string[]), 2));
 	}
 
 	if (coroutine.status(co) !== "dead") {
-		error(debug.traceback(co, "Attempted to yield inside changed event!"), 2);
+		warn(debug.traceback(co, "Attempted to yield inside changed event!", 2));
 	}
 
 	return results as LuaTuple<[unknown, unknown]>;
