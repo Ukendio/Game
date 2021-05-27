59fea2c064d5ff920b1b32e107bafebeebba43af
Marcus <Ukendio@gmail.com>
2021-05-27 07:43:41 +0200

    Added more remotes

--- src/shared/Remotes.ts
+++ src/shared/Remotes.ts
@@ -1,10 +1,19 @@
 import Net from "@rbxts/net";
-import NetDefinitions from "@rbxts/net/out/definitions";
+import { TopicFormat } from "server/core/Council";
 
 const remotes = Net.Definitions.Create({
 	ServerCreateHealthPack: Net.Definitions.ServerToClientEvent<[Model]>(),
 
-	ClientRequestDeploy: NetDefinitions.ClientToServerEvent(),
+	CouncilVoteOn: Net.Definitions.ServerToClientEvent<[TopicFormat]>(),
+	CouncilStopVote: Net.Definitions.ServerToClientEvent(),
+	UICountdown: Net.Definitions.ServerToClientEvent<[number, string, string]>(),
+	UIScoreboardUpdate: Net.Definitions.ServerToClientEvent<[string]>(),
+	RoundStarted: Net.Definitions.ServerToClientEvent(),
+
+	ClientRequestDeploy: Net.Definitions.ServerAsyncFunction<Callback>(),
+
+	ClientAppendVote: Net.Definitions.ClientToServerEvent<[string]>(),
+	ClientChooseTeam: Net.Definitions.ClientToServerEvent<[Team]>(),
 });
 
 export default remotes;
