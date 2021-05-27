f8198b3fa20fb4026359fcb555ef42b1a6ff5524
Marcus <Ukendio@gmail.com>
2021-05-27 09:12:58 +0200

    removed redundancy

--- src/client/core/bootstrap.client.tsx
+++ src/client/core/bootstrap.client.tsx
@@ -1,10 +1,8 @@
-import { StarterPlayer, Players, UserInputService } from "@rbxts/services";
+import { StarterPlayer, Players } from "@rbxts/services";
 import FabricLib from "@rbxts/fabric";
 import Remotes from "shared/remotes";
 import Spirit from "shared/Spirit";
 import Roact from "@rbxts/roact";
-import { RedVignette } from "client/UserInterface/Components/RedVignette";
-import yieldForR15CharacterDescendants from "@rbxts/yield-for-character";
 import { Home } from "client/UserInterface/App/Home";
 
 const player = Players.LocalPlayer;
@@ -17,8 +15,6 @@ const fabric = new FabricLib.Fabric("Example");
 	FabricLib.useTags(fabric);
 	FabricLib.useBatching(fabric);
 	fabric.registerUnitsIn(StarterPlayer.StarterPlayerScripts.TS.units);
-	Players.LocalPlayer.CameraMode = Enum.CameraMode.LockFirstPerson;
-	UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter;
 }
 
 const ServerCreateHealthPack = Remotes.Client.Get("ServerCreateHealthPack");
@@ -34,22 +30,9 @@ gun.mergeBaseLayer({});
 
 Spirit.bind(Enum.KeyCode.M);
 
-yieldForR15CharacterDescendants(player.Character ?? player.CharacterAdded.Wait()[0]).then((rig) => {
-	const handle = Roact.mount(
-		<screengui Key={"Main"} IgnoreGuiInset={true} ResetOnSpawn={false}>
-			<RedVignette />
-		</screengui>,
-		playerGui,
-	);
-
-	rig.Humanoid.Died.Connect(() => {
-		Roact.unmount(handle);
-	});
-
-	Roact.mount(
-		<screengui Key={"HomeScreen"} IgnoreGuiInset={true} ResetOnSpawn={false}>
-			<Home />
-		</screengui>,
-		playerGui,
-	);
-});
+Roact.mount(
+	<screengui Key={"HomeScreen"} IgnoreGuiInset={true} ResetOnSpawn={false}>
+		<Home />
+	</screengui>,
+	playerGui,
+);
