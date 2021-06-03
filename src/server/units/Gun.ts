089f4b98540c2b04dca8c3fedb429716b05845e8
Marcus <Ukendio@gmail.com>
2021-06-03 22:49:59 +0200

    use match

--- src/server/units/Gun.ts
+++ src/server/units/Gun.ts
@@ -1,36 +1,9 @@
 import { Players } from "@rbxts/services";
 import store from "server/core/store";
 import { addKillToPlayer, addDeathToPlayer, addKillToTeam, addDeathToTeam } from "server/core/store/actions";
+import { match } from "shared/rbxts-pattern";
 
 const FIRE_RATE = 1;
-
-function addKill(player: Player, target: BasePart) {
-	const killMap = {
-		["Team Deathmatch"]: () => {
-			if (store.getState().sequence === "intermission") return;
-
-			store.dispatch(addKillToPlayer(player));
-			store.dispatch(addDeathToPlayer(player));
-
-			store.getState().teams.forEach((team) => {
-				if (team.tag === player.Team) {
-					store.dispatch(addKillToTeam(team));
-				} else if (team.tag === Players.GetPlayerFromCharacter(target.Parent)!.Team) {
-					store.dispatch(addDeathToTeam(team));
-				}
-			});
-		},
-
-		["Free For All"]: () => {
-			if (store.getState().sequence === "intermission") return;
-
-			store.dispatch(addKillToPlayer(player));
-			store.dispatch(addDeathToPlayer(player));
-		},
-	};
-
-	return killMap;
-}
 
 const gun: FabricUnits["Gun"] = {
 	name: "Gun",
@@ -69,13 +42,36 @@ const gun: FabricUnits["Gun"] = {
 		function (this) {
 			const damage = this.get("hit");
 			const target = this.get("target");
+			const player = this.get("player");
+
 			if (damage !== undefined && damage !== "Miss" && target !== undefined) {
 				const humanoid = (target.Parent as Model).FindFirstChildOfClass("Humanoid");
 
 				if (humanoid) {
 					humanoid.TakeDamage(tonumber(this.get("hit") as string)!);
 
-					if (humanoid.Health <= 0) addKill(this.get("player"), target)[store.getState().gameMode];
+					if (humanoid.Health <= 0)
+						match(store.getState().gameMode)
+							.with("Team Deathmatch", () => {
+								if (store.getState().sequence === "intermission") return;
+
+								store.dispatch(addKillToPlayer(player));
+								store.dispatch(addDeathToPlayer(player));
+
+								store.getState().teams.forEach((team) => {
+									if (team.tag === player.Team) {
+										store.dispatch(addKillToTeam(team));
+									} else if (team.tag === Players.GetPlayerFromCharacter(target.Parent)!.Team) {
+										store.dispatch(addDeathToTeam(team));
+									}
+								});
+							})
+							.with("Free For All", () => {
+								if (store.getState().sequence === "intermission") return;
+
+								store.dispatch(addKillToPlayer(player));
+								store.dispatch(addDeathToPlayer(player));
+							});
 				}
 			}
 		},
