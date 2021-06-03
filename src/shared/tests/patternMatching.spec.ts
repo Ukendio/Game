1d8799498f11f598e69d7cd8fba66d80d4618815
Marcus <Ukendio@gmail.com>
2021-06-03 22:48:46 +0200

    added math test

--- src/shared/tests/patternMatching.spec.ts
+++ src/shared/tests/patternMatching.spec.ts
@@ -1,25 +1,9 @@
 /// <reference types="@rbxts/testez/globals" />
-import { match, _select } from "shared/rbxts-pattern";
+import { match } from "shared/rbxts-pattern";
 
 type Data = { type: "text"; content: string } | { type: "img"; src: string };
 
 type Result = { type: "ok"; data: Data } | { type: "error"; errorMessage: string };
-
-const getContent = new Promise<Result>((resolve) => {
-	if (math.random(1, 5) > 2) {
-		resolve({
-			type: "ok",
-			data: {
-				type: "text",
-				content: "hello",
-			},
-		});
-	} else
-		resolve({
-			type: "error",
-			errorMessage: "u suck",
-		});
-});
 
 export = () => {
 	describe("pattern", () => {
@@ -38,18 +22,37 @@ export = () => {
 
 			expect(result).to.equal("hello");
 		});
+		it("adds to 15", () => {
+			enum Expressions {
+				Add,
+				Sub,
+				Div,
+				Mul,
+				enumMember,
+			}
+			const expression = Expressions.Add;
+
+			const result = match(expression)
+				.with(Expressions.Add, () => 12 + 3)
+				.with(Expressions.Sub, () => 12 - 3)
+				.with(Expressions.Div, () => 12 / 3)
+				.with(Expressions.Mul, () => 12 * 3)
+				.otherwise(() => error("This shouldn't happen"));
+
+			expect(result).to.equal(15);
+		});
 		it("should throw", () => {
 			const value = {
 				type: "error",
 				errorMessage: "something went wrong",
 			};
 
-			const result = match(value as Result)
-				.with({ type: "ok", data: { type: "text", content: "hello" } }, (result) => result.data.content)
-				.with({ type: "error" }, (result) => error(`Error://${result.errorMessage}`))
-				.run();
-
-			expect(result).to.throw;
+			expect(() =>
+				match(value as Result)
+					.with({ type: "ok", data: { type: "text", content: "hello" } }, (result) => result.data.content)
+					.with({ type: "error" }, (result) => error(`Error://${result.errorMessage}`))
+					.run(),
+			).to.throw;
 		});
 	});
 };
