92c72132279c08f9c995ea9810ca17aa450a02f3
Marcus <Ukendio@gmail.com>
2021-06-08 04:25:28 +0200

    removed client unit factories

--- src/client/core/factory/createGun.ts
+++
@@ -1,7 +0,0 @@
-import { Fabric } from "@rbxts/fabric";
-import { ConfigurableSettings } from "shared/Types";
-
-export function createGun(fabric: Fabric, gunTool: Tool, settings: ConfigurableSettings) {
-	const gun = fabric.getOrCreateUnitByRef("Gun", gunTool);
-	gun.mergeBaseLayer({ configurableSettings: settings });
-}
--- src/client/core/factory/createHealthPack.ts
+++
@@ -1,6 +0,0 @@
-import { Fabric } from "@rbxts/fabric";
-
-export function createHealthPack(fabric: Fabric, healthPack: Model) {
-	const c = fabric.getOrCreateUnitByRef("Heal", healthPack);
-	c.mergeBaseLayer({});
-}
