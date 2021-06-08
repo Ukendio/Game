8ebb148eba5d521ec9fdfbd8bb9071d179f70bdd
Marcus <Ukendio@gmail.com>
2021-06-08 04:33:06 +0200

    renamed event name

--- src/server/core/factory/createMelee.ts
+++ src/server/core/factory/createMelee.ts
@@ -1,7 +1,7 @@
 import { Fabric } from "@rbxts/fabric";
 import Remotes from "shared/remotes";
 
-const ServerCreateKnife = Remotes.Server.Create("ServerCreateKnife");
+const ServerCreateMelee = Remotes.Server.Create("ServerCreateMelee");
 
 export function createMelee(fabric: Fabric, player: Player, melee: Tool) {
 	const backpack = player.WaitForChild("Backpack");
@@ -11,5 +11,5 @@ export function createMelee(fabric: Fabric, player: Player, melee: Tool) {
 	const c = fabric.getOrCreateUnitByRef("Melee", melee);
 	c.mergeBaseLayer({});
 
-	ServerCreateKnife.SendToPlayer(player, melee);
+	ServerCreateMelee.SendToPlayer(player, melee);
 }
