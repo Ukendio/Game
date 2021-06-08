5edc96d66580af3f10be6bfd286bfe9f2d255628
Marcus <Ukendio@gmail.com>
2021-06-08 04:30:36 +0200

    changed path to assets

--- types/ReplicatedStorage.d.ts
+++ types/ReplicatedStorage.d.ts
@@ -1,11 +1,12 @@
 interface ReplicatedStorage extends Instance {
+	assets: Folder & {
+		Heal: Model;
+		Pistol: Tool;
+		HealingSound: Sound;
+		PistolShot: Sound;
+		Knife: Tool;
+	};
 	TS: Folder & {
-		assets: Folder & {
-			Heal: Model;
-			Pistol: Tool;
-			HealingSound: Sound;
-			PistolShot: Sound;
-		};
 		tests: Folder;
 		Architect: Folder & {
 			maps: Folder;
