35d3eb01acb234b45c8eb237d04d3029a5b073f1
Marcus <Ukendio@gmail.com>
2021-06-10 17:53:53 +0200

    remove defaults

--- src/client/units/Luck.ts
+++ src/client/units/Luck.ts
@@ -11,20 +11,11 @@ interface LuckDefinition extends UnitDefinition<"Luck"> {
 
 	ref?: ThisFabricUnit<keyof FabricUnits>;
 
-	defaults?: {
-		foo: string;
-	};
-
 	applyLuck: (this: ThisFabricUnit<"Luck">, layerData: number) => string;
 }
 
 const Luck: LuckDefinition = {
 	name: "Luck",
-
-	onInitialize: function (this) {
-		//MAKE SURE THIS PRINTS BAR
-		print(this.get("foo"));
-	},
 
 	applyLuck: function (this, layerData) {
 		const rng = math.random(0, layerData);
