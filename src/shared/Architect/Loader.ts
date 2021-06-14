ff4acd1ab13d71357de706875d70435cd5989522
Marcus <Ukendio@gmail.com>
2021-06-14 17:17:38 +0200

    TODO: Cache maps

--- src/shared/Architect/Loader.ts
+++ src/shared/Architect/Loader.ts
@@ -1,8 +1,11 @@
 import { ReplicatedStorage, Workspace } from "@rbxts/services";
 
 export function mapLoadAsync(mapName: string) {
-	const map = ReplicatedStorage.TS.Architect.maps.FindFirstChild(mapName) as Model;
-	map.Parent = Workspace;
+	let map = Workspace.FindFirstChild(mapName) as Model;
+	if (map === undefined) {
+		map = ReplicatedStorage.TS.Architect.maps.FindFirstChild(mapName) as Model;
+		map.Parent = Workspace;
+	}
 
 	return map;
 }
