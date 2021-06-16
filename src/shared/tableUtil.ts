081b8f645d9ea3eeb587b95905900cec49269e3c
Marcus <Ukendio@gmail.com>
2021-06-17 00:26:40 +0200

    assertFuzzyEq

--- src/shared/tableUtil.ts
+++ src/shared/tableUtil.ts
@@ -14,3 +14,37 @@ export function getKeys<T extends object>(obj: T) {
 
 	return t;
 }
+
+export function assertFuzzyEq(a: object, b: object) {
+	if (typeOf(a) !== typeOf(b)) {
+		return false;
+	}
+
+	if (typeOf(a) === "table") {
+		const visitedKeys = new Map<unknown, boolean>();
+
+		for (const [k, v] of pairs(a)) {
+			visitedKeys.set(k, true);
+
+			const ok = assertFuzzyEq(v, b[k as never]);
+			if (!ok) {
+				return false;
+			}
+		}
+
+		for (const [k, v] of pairs(b)) {
+			if (!visitedKeys.get(k)) {
+				const ok = assertFuzzyEq(v, a[k as never]);
+				if (!ok) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	if (a === b) return true;
+
+	return false;
+}
