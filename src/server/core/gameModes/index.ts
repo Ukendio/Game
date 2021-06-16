e2543d9e32ba6dd417762e370e4eebe2eb8089fe
Marcus <Ukendio@gmail.com>
2021-06-17 00:24:33 +0200

    add Kill Confirmed

--- src/server/core/gameModes/index.ts
+++ src/server/core/gameModes/index.ts
@@ -1,5 +1,7 @@
-import FFA_WIN_CONDITION from "./freeForAll";
+import FFA_WIN_CONDITION from "./modes/freeForAll";
+import KC_WIN_CONDITION from "./modes/killConfirmed";
 
 export const gameModes = {
 	["Free For All"]: FFA_WIN_CONDITION,
+	["Kill Confirmed"]: KC_WIN_CONDITION,
 };
