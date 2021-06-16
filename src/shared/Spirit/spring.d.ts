2d36a6b29f58e6abfff77487b7b6c0010539a9d0
Marcus <Ukendio@gmail.com>
2021-06-17 01:21:00 +0200

    remove lua spirit

--- src/shared/Spirit/spring.d.ts
+++
@@ -1,7 +0,0 @@
-interface Spring<T> {
-	Update<T extends Vector3 | Vector2 | number>(dt: number, goal: T): T;
-
-	Reset(pos: Vector3 | Vector2 | number): void;
-}
-declare const Spring: new <T>(f: T, p: Vector3 | Vector2 | number) => Spring<T>;
-export = Spring;
