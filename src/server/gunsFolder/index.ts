4725aaebbf3ed4bb5d6804cc89d49133adc4af9e
Marcus <Ukendio@gmail.com>
2021-06-17 00:38:51 +0200

    use map

--- src/server/gunsFolder/index.ts
+++ src/server/gunsFolder/index.ts
@@ -3,23 +3,29 @@ import { Config, Mode } from "shared/Types";
 
 type WeaponsTable = "AK47" | "M16";
 
-export const weaponsTable: Record<WeaponsTable, Config> = {
-	AK47: identity<Config>({
-		fireRate: 1,
-		recoil: 1,
-		maxDistance: 100,
-		mode: Mode.Semi,
-		damage: 1,
-	}),
-	M16: identity<Config>({
-		fireRate: 1,
-		recoil: 1,
-		maxDistance: 100,
-		mode: Mode.Semi,
-		damage: 1,
-	}),
-};
+const weaponsTable = new Map([
+	[
+		"AK47",
+		{
+			fireRate: 1,
+			recoil: 1,
+			maxDistance: 100,
+			mode: Mode.Auto,
+			damage: 1,
+		},
+	],
+	[
+		"M16",
+		{
+			fireRate: 1,
+			recoil: 1,
+			maxDistance: 100,
+			mode: Mode.Semi,
+			damage: 5,
+		},
+	],
+]);
 
 export const getWeaponSettings = (weaponName: WeaponsTable): Option<Config> => {
-	return Option.wrap(weaponsTable[weaponName]);
+	return Option.wrap(weaponsTable.get(weaponName));
 };
