c3c50adf47f05913fddea3f1bd3d0f74862e1231
Marcus <Ukendio@gmail.com>
2021-06-17 00:27:11 +0200

    wrap loading function in option

--- src/shared/Architect/Loader.ts
+++ src/shared/Architect/Loader.ts
@@ -1,11 +1,10 @@
+import { Option } from "@rbxts/rust-classes";
 import { ReplicatedStorage, Workspace } from "@rbxts/services";
 
-export function mapLoadAsync(mapName: string) {
-	let map = Workspace.FindFirstChild(mapName) as Model;
-	if (map === undefined) {
-		map = ReplicatedStorage.TS.Architect.maps.FindFirstChild(mapName) as Model;
+export function loadMapOption(mapName: string) {
+	return Option.wrap(Workspace.FindFirstChild(mapName) as Model).unwrapOrElse(() => {
+		const map = ReplicatedStorage.TS.Architect.maps.FindFirstChild(mapName) as Model;
 		map.Parent = Workspace;
-	}
-
-	return map;
+		return map;
+	});
 }
