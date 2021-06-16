c462f7a1a507e649b2e620ba284df13a4712faef
Marcus <Ukendio@gmail.com>
2021-06-17 00:23:29 +0200

    vote system initial commit

--- src/client/UserInterface/App/Ballot.tsx
+++ src/client/UserInterface/App/Ballot.tsx
@@ -1,20 +1,31 @@
 import Roact from "@rbxts/roact";
 import Vote from "client/UserInterface/Components/Vote";
+import remotes from "shared/remotes";
 import { TopicFormat } from "shared/Types";
 
-interface Props {
+interface State {
 	topic: TopicFormat;
 }
+const CouncilVoteOn = remotes.Client.Get("CouncilVoteOn");
 
-class Ballot extends Roact.Component<Props> {
+class Ballot extends Roact.Component {
+	state: State = {
+		topic: undefined! as TopicFormat,
+	};
+
+	didMount() {
+		CouncilVoteOn.Connect((topic) => {
+			this.setState({ topic: topic });
+		});
+	}
 	render() {
 		return (
 			<frame Size={new UDim2(0, 400, 0, 250)}>
 				<uiscale Scale={0.4} />
 				<uilistlayout FillDirection="Horizontal" Padding={new UDim(0, 15)} />
-				<Vote image={this.props.topic.options[0]} />
-				<Vote image={this.props.topic.options[1]} />
-				<Vote image={this.props.topic.options[2]} />
+				<Vote type={this.state.topic.name} option={this.state.topic.options[0]} />
+				<Vote type={this.state.topic.name} option={this.state.topic.options[1]} />
+				<Vote type={this.state.topic.name} option={this.state.topic.options[2]} />
 			</frame>
 		);
 	}
--- src/client/core/index.client.tsx
+++ src/client/core/index.client.tsx
@@ -4,6 +4,8 @@ registerListenerIn(script.FindFirstChild("listeners")!);
 import { Players, StarterGui } from "@rbxts/services";
 import Roact from "@rbxts/roact";
 import { Home } from "client/UserInterface/App/Home";
+import remotes from "shared/remotes";
+import Ballot from "client/UserInterface/App/Ballot";
 
 const player = Players.LocalPlayer;
 const playerGui = player.WaitForChild("PlayerGui");
