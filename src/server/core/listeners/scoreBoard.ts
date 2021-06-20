6800c987e88f6f22038383f3e273aee96b04634f
Marcus <Ukendio@gmail.com>
2021-06-20 22:07:00 +0200

    [Moved] under services

--- src/server/core/listeners/scoreBoard.ts
+++
@@ -1,8 +0,0 @@
-import { Players } from "@rbxts/services";
-import Remotes from "shared/remotes";
-
-const UIScoreboardUpdate = Remotes.Server.Create("UIScoreboardUpdate");
-
-Players.PlayerRemoving.Connect((player) => {
-	UIScoreboardUpdate.SendToAllPlayersExcept(player, player.Name);
-});
