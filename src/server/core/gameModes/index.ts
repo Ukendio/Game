ac69bb2d88cc2ea7ed21ff2b34bf80de0b81e2a1
Marcus <Ukendio@gmail.com>
2021-06-20 22:04:59 +0200

    [Renamed Path] mode -> modeBuilder

--- src/server/core/gameModes/index.ts
+++ src/server/core/gameModes/index.ts
@@ -1,5 +1,5 @@
-import FFA_WIN_CONDITION from "./modes/freeForAll";
-import KC_WIN_CONDITION from "./modes/killConfirmed";
+import FFA_WIN_CONDITION from "./modeBuilder/freeForAll";
+import KC_WIN_CONDITION from "./modeBuilder/killConfirmed";
 
 export const gameModes = {
 	["Free For All"]: FFA_WIN_CONDITION,
