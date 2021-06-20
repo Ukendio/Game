cee169489e51213f0fa1c697f08bf0c548931030
Marcus <Ukendio@gmail.com>
2021-06-20 22:13:35 +0200

    findSpawn function - helper

---
+++ src/server/services/characterHandler/findSpawn.ts
@@ -0,0 +1,28 @@
+import { State } from "server/core/store/reducer";
+
+export async function findSpawn(state: State, player: Player) {
+	let closestSpawn = undefined! as SpawnLocation;
+
+	let closestMagnitude = undefined! as number;
+
+	state.spawnLocations.forEach((spawnLocation) => {
+		let totalMagnitude = 0;
+		state.deployedPlayers.iter().forEach((player) => {
+			const root = player.Character?.FindFirstChild("HumanoidRootPart") as BasePart;
+			if (root) {
+				totalMagnitude += spawnLocation.Position.sub(root.Position).Magnitude;
+			}
+		});
+
+		if (closestMagnitude === undefined) {
+			closestMagnitude = totalMagnitude;
+		}
+
+		if (totalMagnitude < closestMagnitude) {
+			closestMagnitude = totalMagnitude;
+			closestSpawn = spawnLocation;
+		}
+	});
+
+	player.RespawnLocation = closestSpawn;
+}
