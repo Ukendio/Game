50a267178f41071d805764286c873246c0bac48c
Marcus <Ukendio@gmail.com>
2021-06-20 22:13:46 +0200

    Election service

---
+++ src/server/services/election/index.ts
@@ -0,0 +1,95 @@
+import { OnInit, OnStart, Service } from "@rbxts/flamework";
+import { Vec } from "@rbxts/rust-classes";
+import { gameModes } from "server/core/gameModes";
+import store from "server/core/store";
+import { castVote, createTopic, selectGameMode, startVote, stopVote } from "server/core/store/actions";
+import { match } from "shared/rbxts-pattern";
+import { Events } from "shared/remotes";
+import { getKeys } from "shared/tableUtil";
+import { getVoteOrDefault } from "./getVoteOrDefault";
+
+const events = Events.server;
+
+@Service({
+	loadOrder: 4,
+})
+export class Election implements OnInit, OnStart {
+	constructor() {}
+
+	onInit() {
+		events.connect("clientAppendVote", (player, vote) => {
+			const state = store.getState();
+
+			assert(state.voting === false, "Cannot start a vote when a vote is ongoing");
+			assert(
+				state.topic.options.iter().find((option) => option === vote),
+				"Was not found in votable options",
+			);
+
+			store.dispatch(castVote(player, vote));
+		});
+
+		events.connect("clientAppendVote", (player, vote) => this._castVote(player, vote));
+	}
+
+	onStart() {}
+
+	voteOn(topic: string) {
+		return match(topic)
+			.with("GameMode", async () => {
+				store.dispatch(
+					createTopic({
+						name: "Gamemode",
+						options: Vec.vec(...getKeys(gameModes)),
+					}),
+				);
+
+				store.dispatch(startVote());
+
+				events.councilVoteOn.broadcast(this._serializeTopic());
+
+				await Promise.delay(1).then(() => {
+					store.dispatch(selectGameMode(this._conclude() as keyof typeof gameModes));
+					store.dispatch(stopVote());
+
+					events.councilStopVote.broadcast();
+				});
+			})
+			.with("Map", async () => {
+				store.dispatch(
+					createTopic({
+						name: "Gamemode",
+						options: Vec.vec(...getKeys(gameModes)),
+					}),
+				);
+
+				store.dispatch(startVote());
+
+				events.councilVoteOn.broadcast(this._serializeTopic());
+
+				await Promise.delay(1).then(() => {
+					store.dispatch(selectGameMode(this._conclude() as keyof typeof gameModes));
+					store.dispatch(stopVote());
+
+					events.councilStopVote.broadcast();
+				});
+			});
+	}
+
+	protected _conclude() {
+		const state = store.getState();
+		return getVoteOrDefault(state.votes, state.topic.options);
+	}
+
+	protected _serializeTopic() {
+		const topic = store.getState().topic;
+		return {
+			name: topic.name,
+			options: topic.options.asPtr(),
+		};
+	}
+
+	protected _castVote(player: Player, vote: string) {
+		store.dispatch(castVote(player, vote));
+	}
+}
