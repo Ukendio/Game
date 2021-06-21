7a1c1e5fe96192d6db1512450514286919dfa7cd
Marcus <Ukendio@gmail.com>
2021-06-21 06:41:45 +0200

    team deathmatch inital commit

--- src/server/core/gameModes/index.ts
+++ src/server/core/gameModes/index.ts
@@ -1,7 +1,9 @@
 import FFA_WIN_CONDITION from "./modeBuilder/freeForAll";
 import KC_WIN_CONDITION from "./modeBuilder/killConfirmed";
+import TDM_WIN_CONDITION from "./modeBuilder/freeForAll";
 
 export const gameModes = {
 	["Free For All"]: FFA_WIN_CONDITION,
 	["Kill Confirmed"]: KC_WIN_CONDITION,
+	["Team Deathmatch"]: TDM_WIN_CONDITION,
 };
