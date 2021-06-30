84155d1928949de72a4d109d47f4a86a23e43bfb
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-06-30 21:34:29 +0200

    Initial attempt

---
+++ src/tests/tests.server.lua
@@ -0,0 +1,6 @@
+local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("include"):WaitForChild("RuntimeLib"))
+local TestEZ = TS.import(script, TS.getModule(script, "testez").src)
+local results = TestEZ.TestBootstrap:run({ script.Parent })
+if #results.errors > 0 or results.failureCount > 0 then
+	error("Tests failed!")
+end
