ea0e1512e64be06a9a72df42031e61882ac665af
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-06-30 23:52:01 +0200

    Try different test runner

--- src/server/test.server.ts
+++ src/server/test.server.ts
@@ -2,5 +2,5 @@ import { ReplicatedStorage, RunService } from "@rbxts/services";
 import TestEz from "@rbxts/testez";
 
 if (RunService.IsStudio()) {
-	Promise.delay(1.2).then(() => TestEz.TestBootstrap.run([...ReplicatedStorage.tests.GetChildren()]));
+	TestEz.TestBootstrap.run([...ReplicatedStorage.tests.GetChildren()]);
 }
--- src/tests/test.server.lua
+++
@@ -1,6 +0,0 @@
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
-local TestEZ = TS.import(script, TS.getModule(script, "testez").src)
-local results = TestEZ.TestBootstrap:run({ script.Parent })
-if #results.errors > 0 or results.failureCount > 0 then
-	error("Tests failed!")
-end
