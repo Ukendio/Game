80829ec11e650726e675cdb997981260c64c2911
Marcus <Ukendio@gmail.com>
2021-06-30 21:57:48 +0200

    replace any

--- src/shared/rbxts-pattern/types/Pattern.d.ts
+++ src/shared/rbxts-pattern/types/Pattern.d.ts
@@ -4,7 +4,7 @@ import { Primitives, IsPlainObject } from "./helpers";
 /**
  * GuardValue returns the value guarded by a type guard function.
  */
-export type GuardValue<F> = F extends (value: any) => value is infer b
+export type GuardValue<F> = F extends (value: unknown) => value is infer b
 	? b
 	: F extends (value: infer a) => unknown
 	? a
@@ -53,7 +53,7 @@ type WildCardPattern<a> = a extends number
 
 /**
  * ### Pattern
- * Patterns can be any (nested) javascript value.
+ * Patterns can be unknown (nested) javascript value.
  * They can also be a "wildcards", like `__`.
  */
 export type Pattern<a> =
@@ -61,7 +61,7 @@ export type Pattern<a> =
 	| AnonymousSelectPattern
 	| NamedSelectPattern<string>
 	| GuardPattern<a, a>
-	| NotPattern<a | any>
+	| NotPattern<a | unknown>
 	| WildCardPattern<a>
 	| (a extends Primitives
 			? a
