a1d57262c6d1e0cea1cf4d6324710d4a45f5e1cf
Dionysusnu <39664950+Dionysusnu@users.noreply.github.com>
2021-06-30 22:51:22 +0200

    Fix location

--- src/tests/test.server.lua
+++ src/tests/test.server.lua
@@ -1,4 +1,4 @@
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("include"):WaitForChild("RuntimeLib"))
+local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
 local TestEZ = TS.import(script, TS.getModule(script, "testez").src)
 local results = TestEZ.TestBootstrap:run({ script.Parent })
 if #results.errors > 0 or results.failureCount > 0 then
