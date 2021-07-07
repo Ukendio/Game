bfbd7ab053dce32aa962819df4a0b7ea684bff8a
Marcus <Ukendio@gmail.com>
2021-07-08 01:08:31 +0200

    turn zircon into a controller

---
+++ src/client/controllers/zircon.ts
@@ -0,0 +1,43 @@
+import { Controller, OnInit, OnStart } from "@rbxts/flamework";
+import Log, { Logger } from "@rbxts/log";
+import { Players } from "@rbxts/services";
+import Zircon, { ZirconClient } from "@rbxts/zircon";
+
+@Controller({})
+export class ZirconController implements OnInit, OnStart {
+	onInit() {
+		const configuredLog = Logger.configure()
+			.WriteTo(Zircon.Log.Console())
+			.EnrichWithProperty("Version", PKG_VERSION)
+			.Create();
+
+		Log.SetLogger(configuredLog);
+
+		ZirconClient.BindConsole({
+			EnableTags: true,
+			Keys: [Enum.KeyCode.T],
+		});
+	}
+
+	onStart() {
+		Promise.delay(10).then(() => {
+			Log.Verbose("Verbose message pls");
+			Log.Info("Hello, {Test}! {Boolean} {Number} {Array}", "Test string", true, 10, [1, 2, 3, [4]]);
+			Log.Info("Should be good {Number}", 1);
+			Log.Info(
+				`String {String}, Number {Number}, Boolean {Boolean}, Array: {Array}, Map: {Map}, Instance: {Instance}, Undefined: {Undefined}, None: {None}`,
+				"Hello, World!",
+				1337,
+				true,
+				[1, "two", true],
+				{ value: "hi" },
+				Players.LocalPlayer,
+				undefined,
+			);
+			Log.Debug("test", "testing debug");
+			Log.Warn("test warning lol");
+			Log.Error("test error lol");
+			Log.Fatal("wtf lol");
+		});
+	}
+}
