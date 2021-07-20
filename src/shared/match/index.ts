f6b8ece556f212fb9dd999b2a0cb486da6e7b585
Marcus <Ukendio@gmail.com>
2021-07-20 02:17:57 +0200

    remove prints

--- src/shared/match/index.ts
+++ src/shared/match/index.ts
@@ -105,10 +105,8 @@ const builder = <a, b>(
 			const selected: Record<string, unknown> = {};
 
 			const doesMatch = (value: a) => {
-				print(value);
 				return (
 					patterns.some((pattern) => {
-						print(pattern);
 						return matchPattern(pattern, value, (key, value) => {
 							selected[key] = value;
 						});
