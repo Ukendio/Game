ac96298744d7096bf01e1411d861e7ce07b1cd8d
Marcus <Ukendio@gmail.com>
2021-07-24 02:31:56 +0200

    remove private methods

--- src/server/services/election/index.ts
+++ src/server/services/election/index.ts
@@ -1,19 +1,18 @@
-import { OnInit, OnStart, Service } from "@rbxts/flamework";
+import { OnInit, Service } from "@rbxts/flamework";
 import { Vec } from "@rbxts/rust-classes";
 import { gameModes } from "server/gameModes";
 import store from "server/core/rodux/store";
-import { match } from "shared/match";
 import { getVoteOrDefault } from "./getVoteOrDefault";
 import remotes from "shared/Remotes";
-import { TopicFormat } from "shared/Types";
 import Object from "@rbxts/object-utils";
+import Log from "@rbxts/log";
 
 const serverEvents = remotes.Server;
 
 @Service({
 	loadOrder: 5,
 })
-export class Election implements OnInit, OnStart {
+export class Election implements OnInit {
 	private clientAppendVote = serverEvents.Create("clientAppendVote");
 	private councilVoteOn = serverEvents.Create("councilVoteOn");
 	private councilStopVote = serverEvents.Create("councilStopVote");
@@ -21,10 +20,12 @@ export class Election implements OnInit, OnStart {
 	constructor() {}
 
 	onInit() {
-		this.clientAppendVote.Connect((player, vote) => this._castVote(player, vote));
+		//TODO: sanitize event
+		this.clientAppendVote.Connect((player, vote) => {
+			store.dispatch({ type: "CastVote", player: player, vote: vote });
+			Log.Info("{} has voted on {}", player, vote);
+		});
 	}
-
-	onStart() {}
 
 	async voteOn(topic: string) {
 		if (topic === "GameMode") {
@@ -43,28 +44,18 @@ export class Election implements OnInit, OnStart {
 		}
 
 		store.dispatch({ type: "StartVote" });
-		this.councilVoteOn.SendToAllPlayers(this._serializeTopic(store.getState().topic));
+		this.councilVoteOn.SendToAllPlayers({ name: topic, options: store.getState().topic.options.asPtr() });
 
 		return Promise.delay(1).then(() => {
-			store.dispatch({ type: "SelectGameMode", gameMode: this._conclude() as keyof typeof gameModes });
+			const currentState = store.getState();
+			store.dispatch({
+				type: "SelectGameMode",
+				gameMode: getVoteOrDefault(currentState.votes, currentState.topic.options).expect(
+					"Unexpected error",
+				) as keyof typeof gameModes,
+			});
 			store.dispatch({ type: "StopVote" });
 			this.councilStopVote.SendToAllPlayers();
 		});
-	}
-
-	protected _conclude() {
-		const state = store.getState();
-		return getVoteOrDefault(state.votes, state.topic.options);
-	}
-
-	protected _serializeTopic(topic: TopicFormat) {
-		return {
-			name: topic.name,
-			options: topic.options.asPtr(),
-		};
-	}
-
-	protected _castVote(player: Player, vote: string) {
-		store.dispatch({ type: "CastVote", player: player, vote: vote });
 	}
 }
