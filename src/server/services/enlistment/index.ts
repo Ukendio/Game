84133a1d80c0d153d403a49b26614daf6ac8de61
Marcus <Ukendio@gmail.com>
2021-07-24 02:32:17 +0200

    make methods into private functions

--- src/server/services/enlistment/index.ts
+++ src/server/services/enlistment/index.ts
@@ -1,11 +1,12 @@
 import { Service } from "@rbxts/flamework";
 import store from "server/core/rodux/store";
 
+function lock(tag: Team) {}
+
 @Service({
 	loadOrder: 6,
 })
 export class Enlistment {
-	constructor() {}
 	join(player: Player) {
 		const team = store
 			.getState()
@@ -13,13 +14,7 @@ export class Enlistment {
 			.maxBy((a, b) => a.members.len() - b.members.len())
 			.unwrap();
 
-		this._lock(team.tag);
+		lock(team.tag);
 		store.dispatch({ type: "AddTeammate", player: player, team: team });
 	}
-
-	_ensureLocked() {}
-
-	_lock(team: Team) {}
-
-	_unlock(team: Team) {}
 }
