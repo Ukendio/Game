2165884c6d75b5d65f05199115b04d1f67d8250c
Marcus <Ukendio@gmail.com>
2021-07-24 02:27:47 +0200

    class into functional component

--- src/client/ui/app/Home.tsx
+++ src/client/ui/app/Home.tsx
@@ -1,104 +1,71 @@
 import Roact from "@rbxts/roact";
 import Remotes from "shared/Remotes";
 import { Players, UserInputService } from "@rbxts/services";
-import Log from "@rbxts/log";
 import { getCamera } from "client/core/userCamera";
+import Hooks from "@rbxts/roact-hooks";
+import { Option } from "@rbxts/rust-classes";
+import Log from "@rbxts/log";
 
 const roundStarted = Remotes.Client.WaitFor("roundStarted");
 const deployUser = Remotes.Client.WaitFor("userRequestDeploy");
 
-interface State {
-	status: Color3;
-	visible: boolean;
-	debounce: boolean;
-	mouseConnection: RBXScriptConnection;
-}
 interface Props {}
 
-export class Home extends Roact.Component<Props, State> {
-	state: State = {
-		status: new Color3(1, 0, 0),
-		visible: true,
-		debounce: true,
-		mouseConnection: undefined!,
-	};
+getCamera()
+	.setActorNone()
+	.addBlur(1)
+	.mapErr((errorMessage) => Log.Warn("{}", errorMessage));
 
-	didMount() {
-		roundStarted.then((remote) => {
-			Log.Info("round started for client");
-			remote.Connect(() => {
-				Log.Info("change status ig");
-				this.setState({ status: new Color3(0, 1, 0), visible: this.state.visible });
-			});
-		});
+UserInputService.InputChanged.Connect((inputObject) => {
+	if (inputObject.UserInputType === Enum.UserInputType.MouseMovement) getCamera().move();
+});
 
-		getCamera().setActorNone().addBlur(1);
+const home: Hooks.FC<Props> = (_, { useState }) => {
+	const [status, setStatus] = useState("intermission");
+	const [visibility, setVisibility] = useState(true);
+	const [debounce, bounce] = useState(true);
 
-		this.setState({
-			mouseConnection: UserInputService.InputChanged.Connect((inputObject) => {
-				if (inputObject.UserInputType === Enum.UserInputType.MouseMovement) getCamera().move();
-			}),
-		});
-	}
+	roundStarted.then((remote) => remote.Connect(() => setStatus("started")));
 
-	render(): Roact.Element {
-		return (
-			<frame
-				Key={"MainFrame"}
-				BackgroundTransparency={1}
-				Size={new UDim2(1, 0, 1, 0)}
-				Visible={this.state.visible}
-			>
-				<textbutton
-					Key={"DeployButton"}
-					AnchorPoint={new Vector2(0.5, 0)}
-					BackgroundColor3={this.state.status}
-					BorderSizePixel={0}
-					Position={new UDim2(0.5, 0, 0, 0)}
-					Size={new UDim2(0.3, 0, 0.1, 0)}
-					SizeConstraint={Enum.SizeConstraint.RelativeYY}
-					Text={"DEPLOY"}
-					TextScaled={true}
-					Font={Enum.Font.SourceSansBold}
-					Event={{
-						MouseButton1Down: () => {
-							if (this.state.debounce) {
-								deployUser.then((remote) => {
-									remote.CallServerAsync().then((deployed: boolean | Model) => {
-										if (
-											deployed !== false &&
-											typeIs(deployed, "Instance") &&
-											deployed.IsA("Model")
-										) {
-											getCamera().removeBlur().setActor(deployed);
+	return (
+		<frame Key={"MainFrame"} BackgroundTransparency={1} Size={new UDim2(1, 0, 1, 0)} Visible={visibility}>
+			<textbutton
+				Key={"DeployButton"}
+				AnchorPoint={new Vector2(0.5, 0)}
+				BackgroundColor3={status === "started" ? new Color3(0, 1, 0) : new Color3(1, 0, 0)}
+				BorderSizePixel={0}
+				Position={new UDim2(0.5, 0, 0, 0)}
+				Size={new UDim2(0.3, 0, 0.1, 0)}
+				SizeConstraint={Enum.SizeConstraint.RelativeYY}
+				Text={"DEPLOY"}
+				TextScaled={true}
+				Font={Enum.Font.SourceSansBold}
+				Event={{
+					MouseButton1Down: () => {
+						if (status === "started" && debounce) {
+							bounce(true);
 
-											Players.LocalPlayer.CameraMode = Enum.CameraMode.LockFirstPerson;
-											UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter;
-
-											this.state.mouseConnection.Disconnect();
-
-											this.setState({
-												status: this.state.status,
-												visible: false,
-												debounce: false,
-												mouseConnection: undefined!,
-											});
-										}
-									});
+							deployUser.then((remote) => {
+								remote.CallServerAsync().then((deployed) => {
+									if (deployed) {
+										setVisibility(false);
+									}
 								});
+							});
 
-								Promise.delay(1).then(() =>
-									this.setState({
-										status: this.state.status,
-										visible: this.state.visible,
-										debounce: true,
-									}),
-								);
-							}
-						},
-					}}
-				/>
-			</frame>
-		);
-	}
-}
+							getCamera()
+								.setActor(Option.wrap(Players.LocalPlayer.Character))
+								.expect("character model is undefined")
+								.unwrap()
+								.removeBlur();
+
+							Promise.delay(2).then(() => bounce(true));
+						}
+					},
+				}}
+			/>
+		</frame>
+	);
+};
+
+export = new Hooks(Roact)(home);
