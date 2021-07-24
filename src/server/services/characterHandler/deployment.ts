04d78595b94f0efb5a1c44e4d59d9fe7215de209
Marcus <Ukendio@gmail.com>
2021-07-24 02:28:34 +0200

    sanitize callback

--- src/server/services/characterHandler/deployment.ts
+++ src/server/services/characterHandler/deployment.ts
@@ -1,6 +1,7 @@
 import { OnInit, Service } from "@rbxts/flamework";
 import Remotes from "shared/Remotes";
 import store from "server/core/rodux/store";
+import Log from "@rbxts/log";
 const userRequestDeploy = Remotes.Server.Create("userRequestDeploy");
 
 @Service({ loadOrder: 5 })
@@ -9,10 +10,21 @@ export class Deployment implements OnInit {
 		userRequestDeploy.SetCallback((player) => {
 			const state = store.getState();
 			if (state.sequence === "started") {
-				store.dispatch({ type: "Deploy", player: player });
-				if (player.Character) {
-					return player.Character;
-				}
+				return state.deployedPlayers
+					.iter()
+					.find((current) => current === player)
+					.match(
+						(player) => {
+							store.dispatch({ type: "Deploy", player: player });
+							Log.Info("Deployed player: {}", player);
+
+							return true;
+						},
+						() => {
+							Log.Warn("Couldn't find player");
+							return false;
+						},
+					);
 			}
 
 			return false;
