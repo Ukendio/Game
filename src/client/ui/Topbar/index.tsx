b0ce338b21d305ce69b95dfa13e43bb9232edcc4
Marcus <Ukendio@gmail.com>
2021-07-28 17:18:48 +0200

    move play into separate logical type

--- src/client/ui/Topbar/index.tsx
+++ src/client/ui/Topbar/index.tsx
@@ -6,12 +6,6 @@ const TopBar: Hooks.FC = () => {
 	return (
 		<frame BorderSizePixel={0} Size={new UDim2(1, 0, 0, 60)} BackgroundTransparency={1}>
 			<Play />
-			<frame
-				Size={new UDim2(1, 0, 0, 1)}
-				BorderSizePixel={0}
-				BackgroundTransparency={0.75}
-				BackgroundColor3={Color3.fromRGB(237, 237, 237)}
-			/>
 		</frame>
 	);
 };
