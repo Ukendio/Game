7acaf442911175c717d8562de0d6a8e42db36e82
Marcus <Ukendio@gmail.com>
2021-07-28 23:15:56 +0200

    remove never

--- src/client/ui/Topbar/buttons/Play.tsx
+++ src/client/ui/Topbar/buttons/Play.tsx
@@ -27,8 +27,8 @@ const Play: Hooks.FC = (_, { useState }) => {
 	return (
 		<Button
 			Visible={visibility}
-			hoverOff={ColourScheme.Black as never}
-			hoverOn={ColourScheme.Yellow as never}
+			hoverOff={ColourScheme.Black}
+			hoverOn={ColourScheme.Yellow}
 			Text="PLAY"
 			handler={() => {
 				match([status, debounce])
