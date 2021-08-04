6a4df65ea55bcffda920752f040cb20c8c2121f6
Marcus <Ukendio@gmail.com>
2021-08-04 21:13:08 +0200

    remove redundant variable

--- src/tests/Match.spec.ts
+++ src/tests/Match.spec.ts
@@ -6,22 +6,22 @@ export = () => {
 		it("number is in set", () => {
 			const number = 5;
 
-			const result = match(number)
-				.numberSet([1, 3, 5, 7, 11], (value) => `${value} is in array`)
-				.with(__, (value) => `${value} is NOT in array`)
-				.exhaustive();
-
-			expect(result).to.equal(`5 is in array`);
+			expect(
+				match(number)
+					.numberSet([1, 3, 5, 7, 11], (value) => `${value} is in array`)
+					.with(__, (value) => `${value} is NOT in array`)
+					.exhaustive(),
+			).to.equal(`5 is in array`);
 		});
 
 		it("it is a Teen", () => {
 			const number = 13;
 
-			const result = match(number)
-				.numberRange([13, 19], (value) => `${value} is a teen number`)
-				.run();
-
-			expect(result).to.equal("13 is a teen number");
+			expect(
+				match(number)
+					.numberRange([13, 19], (value) => `${value} is a teen number`)
+					.with(__, (value) => `${value} was not in set`),
+			).to.equal("13 is a teen number");
 		});
 	});
 
@@ -52,12 +52,12 @@ export = () => {
 	it("2 is even", () => {
 		const isOdd = (x: number) => x % 2 === 1;
 
-		const result = match({ x: 2 })
-			.with({ x: when(isOdd) }, ({ x }) => `${x} is odd`)
-			.with(__, ({ x }) => `${x} is even`)
-			.exhaustive();
-
-		expect(result).to.equal("2 is even");
+		expect(
+			match({ x: 2 })
+				.with({ x: when(isOdd) }, ({ x }) => `${x} is odd`)
+				.with(__, ({ x }) => `${x} is even`)
+				.exhaustive(),
+		).to.equal("2 is even");
 	});
 
 	it("is a prime number", () => {
