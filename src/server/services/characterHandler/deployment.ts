99f12365a70d73eaaa2c9cfdcda44be63cd7c5c9
Marcus <Ukendio@gmail.com>
2021-09-14 17:27:43 +0200

    inverse evaluation

--- src/server/services/characterHandler/deployment.ts
+++ src/server/services/characterHandler/deployment.ts
@@ -2,32 +2,27 @@ import { OnInit, Service } from "@flamework/core";
 import Remotes from "shared/Remotes";
 import store from "server/core/rodux/store";
 import Log from "@rbxts/log";
+import { Sequence } from "shared/Types";
 const userRequestDeploy = Remotes.Server.Create("userRequestDeploy");
 
 @Service({ loadOrder: 5 })
 export class Deployment implements OnInit {
 	onInit() {
-		userRequestDeploy.SetCallback((player) => {
-			if (store.getState().round.sequence === "started") {
+		userRequestDeploy.Connect((player) => {
+			if (store.getState().round.sequence === Sequence.Started) {
 				return store
 					.getState()
 					.dispatcher.deployedPlayers.iter()
 					.find((current) => current === player)
 					.match(
 						(player) => {
-							store.dispatch({ type: "Deploy", player: player });
-							Log.Info("Deployed player: {}", player);
-
-							return true;
+							Log.Warn("{} has already been deployed", player.Name);
 						},
 						() => {
-							Log.Warn("Couldn't find player");
-							return false;
+							store.dispatch({ type: "Deploy", player });
 						},
 					);
 			}
-
-			return false;
 		});
 	}
 }
