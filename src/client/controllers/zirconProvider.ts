380c9606bbe6740471b505cb62c53ee09d2895aa
Marcus <Ukendio@gmail.com>
2021-09-15 01:35:11 +0200

    remove noise from console

--- src/client/controllers/zirconProvider.ts
+++ src/client/controllers/zirconProvider.ts
@@ -1,9 +1,9 @@
-import { Controller, OnInit, OnStart } from "@flamework/core";
+import { Controller, OnInit } from "@flamework/core";
 import Log, { Logger } from "@rbxts/log";
 import Zircon, { ZirconClient } from "@rbxts/zircon";
 
 @Controller({})
-export class ZirconProvider implements OnInit, OnStart {
+export class ZirconProvider implements OnInit {
 	onInit() {
 		const configuredLog = Logger.configure()
 			.WriteTo(Zircon.Log.Console())
@@ -15,17 +15,6 @@ export class ZirconProvider implements OnInit, OnStart {
 		ZirconClient.BindConsole({
 			EnableTags: true,
 			Keys: [Enum.KeyCode.T],
-		});
-	}
-
-	onStart() {
-		Promise.delay(10).then(() => {
-			Log.Verbose("Verbose message pls");
-			Log.Info("I am {}, but you can call me {}", "Marcus", "Ukendio");
-			Log.Debug("test", "testing debug");
-			Log.Warn("test warning lol");
-			Log.Error("test error lol");
-			Log.Fatal("wtf lol");
 		});
 	}
 }
